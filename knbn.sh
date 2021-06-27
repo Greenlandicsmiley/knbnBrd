b0f5226601b9d910fc631d4cd3ad1d2f7f07b25c
Thomas Petersen <67438283+Greenlandicsmiley@users.noreply.github.com>
2021-06-27 20:11:43 -0200

    Update knbn.sh

--- knbn.sh
+++ knbn.sh
@@ -5,22 +5,17 @@ knbn_board="$knbn_directory/board"
 LICENSE="$knbn_directory/LICENSE"
 NOTICE="$knbn_directory/NOTICE"
 
-check_if_longest_string() { #Checks if string from 1st argument is longer than the last string
-    [[ ${#1} -gt $longest_string ]] && longest_string=${#1}
-}
-
 get_column() { #Gets file for selected column
     file="$(< $knbn_board/"${1,,}")"
     file="${file// /_}"
-    [[ $2 == "," ]] && file="${file//,/_}"
+    [[ "$2" == "," ]] && file="${file//,/_}"
 }
 
 get_lsl() { #Gets longest line in selected column
     get_column "${1,,}" #Gets file for selected column
     for task in $file; do
         text="|   ${task} "
-        [[ ${#text} -gt $lsL ]] && \
-            lsL=${#text}
+        [[ ${#text} -gt $lsL ]] && lsL=${#text}
     done
 }
 
@@ -61,41 +56,50 @@ regenerate_numbers() { #Regenerate line numbers in given column
     done
 }
 
-#Delete all notes in a task from a column: rm_note "task ID" "target"
-#move all notes in a task to a note: rm_note "task ID" "old column" "new column"
+#Delete all notes in a task from a column: rm_notes task_id target
+#move all notes in a task to a note: rm_notes task_id old_column new_column
 rm_notes() {
-    check_next_line="$(sed -n "${1}p" "$knbn_board/${2}")"
+    check_next_line="$(sed -n "${1}p" "$knbn_board/$2")"
     while [[ ${check_next_line%,-,*} == "," ]]; do
-        ! [[ -z "${3}" ]] && printf "%s\\n" "${check_next_line}" >> "$knbn_board/${3}" #Insert note into new column
-        sed -i "/${2}:,/d" "$knbn_board/${2}" #Delete note from old column
-        check_next_line="$(sed -n "${1}p" "$knbn_board/${2}")"
+        ! [[ -z "$3" ]] && printf "%s\\n" "$check_next_line" >> "$knbn_board/$3" #Insert note into new column
+        sed -i "${1}d" "$knbn_board/$2" #Delete note from old column
+        check_next_line="$(sed -n "${1}p" "$knbn_board/$2")"
     done
 }
 
-case $1 in
-    "add") #Add tasks
+[[ "$1" != "-"* ]] && func="${1,,}" #To allow things like knbn ADD, or knbn Add, or knbn aDd
+case $func in
+    "add"|"-a"|"--add") #Syntax to add: knbn add column description
         if ! [[ -z "$3" ]]; then
-            add_column="${3,,}"
+            add_column="${2,,}"
             add_column="${add_column// /_}"
-            printf "%s\\n" "tmp:,$2" >> "$knbn_board/${add_column}" #Add task to column specified by 3rd argument, turns any space into _
+            printf "%s\\n" "tmp:,$3" >> "$knbn_board/${add_column}" #Add task to column specified by 3rd argument, turns any space into _
             regenerate_numbers "${add_column}"
-            $0 "ls" "${add_column}"
+            knbn "ls" "${add_column}"
         else
-            printf "%s\\n" "tmp:,$2" >> "$knbn_board/nocat" #Add task to nocat if no 3rd argument given
+            printf "%s\\n" "tmp:,$3" >> "$knbn_board/nocat" #Add task to nocat if no 3rd argument given
             regenerate_numbers "nocat"
-            $0 "ls" "nocat"
+            knbn "ls" "nocat"
         fi
     ;;
-    "nt")
+    "nt"|"-n"|"--note") #Syntax: knbn nt column task_id description
         if ! [[ -z "$2" && -z "$3" && -z "$4" ]]; then
             add_column="${2,,}"
             add_column="${add_column// /_}"
-            note="  - $4"
-            new_line="$(grep "${3}:," "$knbn_board/${add_column}")\\n,,-,${4}"
-            sed -i "/${3}:,/s_.*_${new_line}_" "$knbn_board/${add_column}"
+            task_line="$(grep -n "${3}:," "$knbn_board/$add_column")"
+            task_line="${task_line%:*:,*}"
+            task_line=$(( task_line + 1 ))
+            check_next_line="$(sed -n "${task_line}p" "$knbn_board/$add_column")"
+            while [[ ${check_next_line%,-,*} == "," ]]; do
+                task_line=$(( task_line + 1 ))
+                check_next_line="$(sed -n "${task_line}p" "$knbn_board/$add_column")"
+            done
+            task_line=$(( task_line - 1 ))
+            new_line="$(sed -n "${task_line}p" "$knbn_board/$add_column")\\n,,-,$4"
+            sed -i "${task_line}s_.*_${new_line}_" "$knbn_board/$add_column"
         fi
     ;;
-    "ls") #List tasks
+    "ls"|"-l"|"--list") #Syntax: knbn ls, knbn ls column
         if ! [[ -z $(dir "$knbn_board") ]]; then #Check if board is empty
             lsL=0
             if [[ -z "$2" ]]; then
@@ -118,97 +122,139 @@ case $1 in
             printf "%s\\n" "The board is empty"
         fi
     ;;
-    "mv") #Move a file
-        if ! [[ -z "$3" && -z "$4" ]]; then #Check if 3rd and 4th arguments are empty
-            old_column="${3,,}"
+    "mv"|"-m"|"--move") #Syntax: knbn mv old_column new_column, task_id
+        if ! [[ -z "$2" && -z "$3" && -z "$4" ]]; then #Check if 3rd and 4th arguments are empty
+            old_column="${2,,}"
             old_column="${old_column// /_}"
-            new_column="${4,,}"
+            new_column="${3,,}"
             new_column="${new_column// /_}"
             if [[ -f "$knbn_board/$old_column" ]]; then #Check if old column exists
-                if grep -q "$2:," "$knbn_board/$old_column"; then #Check if task exists in old column
-                    temporary_line="$(grep "$2:," "$knbn_board/$old_column")" #Get task string excluding the line nr
+                if grep -q "$4:," "$knbn_board/$old_column"; then #Check if task exists in old column
+                    temporary_line="$(grep "$4:," "$knbn_board/$old_column")" #Get task string excluding the line nr
+                    task_line=$(grep -n "$4:," "$knbn_board/$old_column")
+                    task_line=${task_line%:*:,*}
                     printf "%s\\n" "${temporary_line/*:,/tmp:,}" >> "$knbn_board/$new_column" #Insert task into new column with temporary ID
-                    sed -i "/${2}:,/d" "$knbn_board/$old_column" #Delete task from old column
-                    rm_notes "$2" "$old_column" "$new_column"
+                    sed -i "/${4}:,/d" "$knbn_board/$old_column" #Delete task from old column
+                    rm_notes "$task_line" "$old_column" "$new_column"
                     file="$(< "$knbn_board/$old_column")"
                     if [[ -z $file ]]; then #Check if old column is empty
                         rm "$knbn_board/$old_column" #Delete column
-                        regenerate_numbers "$new_column" #Regenerate line nr in new column
-                        $0 "ls" "$new_column"
+                        regenerate_numbers "$new_column" #Regenerate line IDs in new column
+                        knbn "ls" "$new_column"
                     else
-                        regenerate_numbers "$old_column" #Regenerate line nr in old column
-                        regenerate_numbers "$new_column" #Regenerate line nr in new column
-                        $0 "ls" "$old_column,$new_column"
+                        regenerate_numbers "$old_column" #Regenerate line IDs in old column
+                        regenerate_numbers "$new_column" #Regenerate line IDs in new column
+                        knbn "ls" "$old_column,$new_column"
                     fi
                 fi
             fi
         fi
     ;;
-    "rm") #Delete a note if 3rd argument has been set
-        if ! [[ -z "$3" ]]; then
-            rm_column="${3,,}"
+    "rm"|"-d"|"--remove") #Syntax: knbn rm column task_id
+        if ! [[ -z "$2" &&  -z "$2" ]]; then
+            rm_column="${2,,}"
             rm_column="${rm_column// /_}"
-            grep -q "$2:," "$knbn_board/$rm_column" && sed -i "/${2}:,/d" "$knbn_board/$rm_column" #Check if ID in selected column exists
-            rm_notes "$2" "$rm_column"
+            grep -q "$3:," "$knbn_board/$rm_column" && sed -i "/${3}:,/d" "$knbn_board/$rm_column" #Check if ID in selected column exists
+            rm_notes "$3" "$rm_column"
             file="$(< $knbn_board/"$rm_column")"
             if [[ -z $file ]]; then
                 rm "${knbn_board:?}/$rm_column" #Delete file if column is empty
             else
                 regenerate_numbers "$rm_column" #Regenerate line numbers
-                $0 "ls" "$rm_column"
+                knbn "ls" "$rm_column"
             fi
         fi
     ;;
-    "ntrm") #Delete a note
+    "rmnt"|"-r"|"--remove-note") #Syntax: knbn rmnt column task_id not_offset (how many lines down the note is)
         if ! [[ -z "$2" && -z "$3" && -z "$4" ]]; then
             column_name="${2,,}"
             column_name="${column_name// /_}"
-            if grep -q "$3:," "$knbn_board/${column_name}"; then
-                offset_line=$(( $3 + $4 ))
-                get_note="$(sed -n "${offset_line}p" "$knbn_board/${column_name}")"
-                if [[ ${get_note%,-,*} == "," ]]; then
-                    sed -i "${offset_line}d" "$knbn_board/${column_name}"
+            if grep -q "$3:," "$knbn_board/$column_name"; then
+                task_line=$(grep -n "${3}:," "$knbn_board/$column_name")
+                task_line=${task_line%:*:,*}
+                note_line=$(( task_line + $4 ))
+                get_task="$(sed -n "${note_line}p" "$knbn_board/$column_name")"
+                if [[ ${get_task%,-,*} == "," ]]; then
+                    sed -i "${note_line}d" "$knbn_board/$column_name"
                 fi
             fi
         else
             printf "%s\\n" "Syntax: knbn ntrm [column] [task ID] [offset from task ID]"
         fi
     ;;
-    "wipe") #Wipe a board if knbn_board has been set, if not fail
+    "wipe"|"-w"|"--wipe") #Syntax: knbn wipe column
         wipe_column="${2,,}"
-        [[ -f "$knbn_board/${wipe_column// /_}" ]] && \
-            rm "$knbn_board/${wipe_column// /_}"
+        [[ -f "$knbn_board/${wipe_column// /_}" ]] && rm "$knbn_board/${wipe_column// /_}"
     ;;
-    "--help"|"help") #Display available commands
-        longest_string=0
-        knbnandc="Add a task to a column:,knbn add \"[description]\" [column]"; check_if_longest_string "${knbnandc%,*}"
-        knband="Add a task:,knbn add \"[description]\""; check_if_longest_string "${knband%,*}"
-        knbnlsc="List tasks in a column:,knbn ls [column]"; check_if_longest_string "${knbnlsc%,*}"
-        knbnls="List all tasks:,knbn ls"; check_if_longest_string "${knbnls%,*}"
-        knbnmvnon="Move a task:,knbn mv [number] [old column] [new column]"; check_if_longest_string "${knbnmvnon%,*}"
-        knbnrmnc="Delete a task from a column:,knbn rm [number] [column]"; check_if_longest_string "${knbnrmnc%,*}"
-        knbnwc="Delete all tasks in a column:,knbn wipe [column]"; check_if_longest_string "${knbnwc%,*}"
+    "backup"|"-b"|"--backup") #Syntax: knbn backup column path_to_location
+        if ! [[ -z "$2" && -z "$3" ]]; then
+            if [[ "$2" == "*" || "$2" == "." ]]; then
+                cp -r "$knbn_board/." "$3"
+                printf "%s" "All columns have been saved to $3"
+            else
+                cp "$knbn_board/$2" "$3"
+                printf "%s" "$2 has been saved to $3"
+            fi
+        fi
+    ;;
+    "merge"|"-M"|"--merge")
+        if ! [[ -z "$2" ]]; then
+            read -s -p "Are you sure you want to merge with backup? This will insert all tasks into their respective columns. (y/N)" -n 1 CONFIRM
+            printf "%s\\n" ""
+        fi
+    ;;
+    "restore"|"-R"|"--restore")
+        if ! [[ -z "$2" ]]; then
+            read -s -p "Are you sure you want to restore from backup? This will remove everything from the board. (y/N)" -n 1 CONFIRM
+            printf "%s\\n" ""
+            [[ ${CONFIRM,,} == "y" ]] && \
+                rm -rf "${knbn_board:?}/*" && \
+                cp -r "$2" "$knbn_board"
+        fi
+    ;;
+    "help"|"-h"|"--help") #Display available commands
+        knbn_list+=("add, -a, --add [column] [description]:,Add a task to a column")
+        knbn_list+=("add, -a, --add [column] [description]:,Add a task to a column")
+        knbn_list+=("mv, -m, --move [old column] [new column] [task id]:,Move a task")
+        knbn_list+=("rm, -d, --remove [column] [task id]:,Delete a task from a column")
+        knbn_list+=("ls, -l, --list [column],[column],etc:,List tasks in a column")
+        knbn_list+=("ls, -l, --list:,List all tasks")
+
+        knbn_list+=("nt, -n, --note [column] [task id] [description]:,Add a note to a task")
+        knbn_list+=("rmnt, -r, --remove-note [column] [task id] [line offset]:,Delete a note from a task")
+
+        knbn_list+=("wipe, -w, --wipe [column]:,Delete all tasks in a column")
+
+        knbn_list+=("backup, -b, --backup [column] [backup location]:,Backup a column")
+        knbn_list+=("backup, -b, --backup [* OR .] [backup location]:,Backup the board")
+        knbn_list+=("merge, -M, --merge [backup location]:,Merge board with backup")
+        knbn_list+=("restore, -R, --restore [backup location]:,Restore from backup")
+
+        knbn_list+=("uninstall, -u, --uninstall:,Uninstall knbnBrd")
+        knbn_list+=("license, -L, --license:,Print license")
+        knbn_list+=("notice, -N, --notice:,Print GPLv3 notice")
+        knbn_list+=("help, -h, --help:,Print this")
+        knbn_list+=("examples, -e, --examples:,Print examples")
+
         printf "\\n"
-        printf "\\x1B[32m%${longest_string}s" "${knbnandc%,*}"; printf "%b" "\\x1B[37m${knbnandc#*:,}\\n"
-        printf "\\x1B[32m%${longest_string}s" "${knband%,*}"; printf "%b" "\\x1B[37m${knband#*:,}\\n"
-        printf "\\x1B[35m%${longest_string}s" "${knbnlsc%,*}"; printf "%b" "\\x1B[37m${knbnlsc#*:,}\\n"
-        printf "\\x1B[35m%${longest_string}s" "${knbnls%,*}"; printf "%b" "\\x1B[37m${knbnls#*:,}\\n"
-        printf "\\x1B[34m%${longest_string}s" "${knbnmvnon%,*}"; printf "%b" "\\x1B[37m${knbnmvnon#*:,}\\n"
-        printf "\\x1B[31m%${longest_string}s" "${knbnrmnc%,*}"; printf "%b" "\\x1B[37m${knbnrmnc#*:,}\\n"
-        printf "\\x1B[31m%${longest_string}s" "${knbnwc%,*}"; printf "%b" "\\x1B[37m${knbnwc#*:,}\\n"
-        printf "\\n"
+        for command in "${knbn_list[@]}"; do
+            command_flags="${command%:,*}"
+            command_description="${command#*:,}"
+            printf "%s\\n" "  $command_flags"
+            printf "%s\\n" "    $command_description"
+        done
     ;;
-    "--uninstall"|"uninstall")
-        rm -rf /opt/knbn
-        rm /usr/bin/knbn
+    "uninstall"|"-u"|"--uninstall") #Uninstall knbnBrd entirely
+        rm -rf /opt/knbnBrd
+        rm /usr/bin/knbnBrd
     ;;
-    "--license"|"license") #Display license
+    "license"|"-L"|"--license") #Display license
         file="$(< $LICENSE)"
         printf "\\n%s\\n" "$file" #Prints the license
     ;;
-    *) #If no option display notice
+    "notice"|"-N"|"--notice"|*) #If no option display notice
         file="$(< $NOTICE)"
         printf "\\n%s\\n" "$file" #Prints the notice
-        printf "\\n%s\\n\\n" "Type knbn license to view the entire license, knbn help to view options"
+        printf "\\n%s\\n\\nv1.1\\n\\n" "Type knbn license to view the entire license, knbn help to view options"
     ;;
 esac
