bd30a8ea3e19005d1945b3c75ea2b20f07f3dfcf
Thomas Petersen <67438283+Greenlandicsmiley@users.noreply.github.com>
2021-06-29 16:03:38 -0200

    Compacting code and some fixes
    Removed repitition
    Fixed nocat bug
    Fixed empty arg checks

--- knbn.sh
+++ knbn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-knbn_directory="/opt/knbnBrd"
+knbn_directory="~/.local/share/knbnBrd"
 knbn_board="$knbn_directory/board"
 LICENSE="$knbn_directory/LICENSE"
 NOTICE="$knbn_directory/NOTICE"
@@ -29,149 +29,131 @@ rm_notes() {
     done
 }
 
+arg_column="${2,,}"
+arg_column="${arg_column// /_}"
+
+task_line="$(grep -n "$3> " "$knbn_board/$arg_column")"
+task_line=${task_line:0:3}
+task_line=${task_line%:*}
+
 case $1 in
     "add"|"-a"|"--add")
-        add_column="${2// /_}" #Replace all spaces with an underscore to prevent catastrophic failure
-        [[ -z "$3" ]] && add_column="nocat" #Default to nocat if 3rd argument is empty
-        printf "%s\\n" "tmp> $3" >> "$knbn_board/${add_column,,}" #Add task to column
-        regenerate_numbers "$add_column" #Generate ids
+        add_string="$3"
+        [[ -z "$3" ]] && arg_column="nocat" && add_string="$2"
+        printf "%s\\n" "tmp> $add_string" >> "$knbn_board/$arg_column"
+        regenerate_numbers "$arg_column"
         knbn "ls"
     ;;
     "nt"|"-n"|"--note")
-        if ! [[ -z "$2" && -z "$3" && -z "$4" ]]; then
-            nt_column="${2,,}" #Make all characters lowercase
-            nt_column="${nt_column// /_}" #Replace all spaces with an underscore to prevent catastrophic failure
-            task_line="$(grep -n "${3}> " "$knbn_board/$nt_column")" #Get entire string prepended by line nr
-            task_line="${task_line:0:3}" #Extract line number from string
-            task_line=$(( ${task_line%:*} + 1 ))
-            check_next_line="$(sed -n "${task_line}p" "$knbn_board/$nt_column")" #Get string of next line
-            while [[ ${check_next_line:0:4} == "  - " ]]; do #Check if string is a note
-                task_line=$(( task_line + 1 )) 
-                check_next_line="$(sed -n "${task_line}p" "$knbn_board/$nt_column")"
-            done
-            task_line=$(( task_line - 1 ))
-            replace_line="$(sed -n "${task_line}p" "$knbn_board/$nt_column")"
-            sed -i "${task_line}s_.*_${replace_line}\\n  - ${4}_" "$knbn_board/$nt_column" #Add note to line
-            knbn "ls"
-        fi
+        [[ -z "$2" || -z "$3" || -z "$4" ]] && exit 1
+
+        task_line=$(( task_line + 1 ))
+        check_next_line="$(sed -n "${task_line}p" "$knbn_board/$arg_column")" #Get string of next line
+        while [[ ${check_next_line:0:4} == "  - " ]]; do #Check if string is a note
+            task_line=$(( task_line + 1 )) 
+            check_next_line="$(sed -n "${task_line}p" "$knbn_board/$arg_column")"
+        done
+        task_line=$(( task_line - 1 ))
+        replace_line="$(sed -n "${task_line}p" "$knbn_board/$arg_column")"
+        sed -i "${task_line}s_.*_${replace_line}\\n  - ${4}_" "$knbn_board/$arg_column" #Add note to line
+        knbn "ls"
     ;;
     "ls"|"-l"|"--list")
-        if ! [[ -z $(dir "$knbn_board") ]]; then #Check if board is empty
-            ls_column="${2// /_}" #Replace all spaces with an underscore to prevent catastrophic failure
-            ls_column="${ls_column,,}" #Make all characters lowercase
-            [[ -z "$ls_column" ]] && ls_column="$(dir $knbn_board)" #Default to all columns if 2nd argument is empty
-            for column in ${ls_column//,/ }; do
-                file="$(< "$knbn_board/$column")" #Get file contents of column
-                for task in ${file// /<}; do
-                    text="|   $task "
-                    [[ ${#text} -gt $lsL ]] && lsL=${#text} #Check if line is longest
-                done
-                text="| $column: "
-                [[ ${#text} -gt $lsL ]] && lsL=${#text}
+        [[ -z $(dir "$knbn_board") ]] && printf "%s\\n" "The board is empty" && exit 0
+        [[ -z "$arg_column" ]] && arg_column="$(dir $knbn_board)" #Default to all columns if 2nd argument is empty
+        for column in ${arg_column//,/ }; do
+            file="$(< "$knbn_board/$column")"
+            for task in ${file// /<}; do
+                text="|   $task "
+                [[ ${#text} -gt $lsL ]] && lsL=${#text} #Check if line is longest
             done
-            printf -v "border" "%-${lsL}s+" "+" #Insert border string into variable
-            for column in ${ls_column//,/ }; do
-                printf "%s\\n" "${border// /-}" #Print border
-                printf "%-${lsL}s|\\n" "| ${column//_/ }:" #Print column name
-                file="$(< "$knbn_board/$column")" #Get file contents of column
-                for task in ${file// /<}; do
-                    task="${task//>/:}"
-                    printf "%-${lsL}s|\\n" "|   ${task//</ }" #Print task and note
-                done
-            done
+            text="| $column: "
+            [[ ${#text} -gt $lsL ]] && lsL=${#text} #Prevents padding from being out of line if column name is shorter than the tasks
+        done
+        printf -v "border" "%-${lsL}s+" "+"
+        for column in ${column//,/ }; do
             printf "%s\\n" "${border// /-}"
-        else
-            printf "%s\\n" "The board is empty"
-        fi
+            printf "%-${lsL}s|\\n" "| ${column//_/ }:"
+            file="$(< "$knbn_board/$column")"
+            for task in ${file// /<}; do
+                task="${task//>/:}"
+                printf "%-${lsL}s|\\n" "|   ${task//</ }"
+            done
+        done
+        printf "%s\\n" "${border// /-}"
     ;;
     "mv"|"-m"|"--move")
-        if ! [[ -z "$2" && -z "$3" && -z "$4" ]]; then #Check if 3rd and 4th arguments are empty
-            old_column="${2,,}"
-            old_column="${old_column// /_}"
-            new_column="${3// /_}"
-            if [[ -f "$knbn_board/$old_column" ]]; then #Check if old column exists
-                if grep -q "$4> " "$knbn_board/$old_column"; then #Check if task exists in old column
-                    task_string="$(grep "$4> " "$knbn_board/$old_column")" #Get task string excluding the line nr
-                    task_line=$(grep -n "$4> " "$knbn_board/$old_column")
-                    task_line=${task_line:0:3} #Extract line nr
-                    printf "%s\\n" "${task_string/*> /tmp> }" >> "$knbn_board/${new_column,,}" #Insert task into new column with temporary ID
-                    rm_notes "$((${task_line%:*}+1))" "$old_column" "${new_column,,}"
-                    sed -i "/${4}> /d" "$knbn_board/$old_column" #Delete task from old column
-                    file="$(< "$knbn_board/$old_column")"
-                    [[ -z $file ]] && rm "${knbn_board:?}/$old_column"
-                    regenerate_numbers "$old_column"
-                    regenerate_numbers "$new_column"
-                    knbn "ls"
-                fi
-            fi
-        fi
+        [[ -z "$2" || -z "$3" || -z "$4" ]] && exit 1
+
+        new_column="${3// /_}"
+
+        ! [[ -f "$knbn_board/$arg_column" ]] || ! grep -q "$4> " "$knbn_board/$arg_column" && exit 1
+
+        task_string="$(grep -n "$4> " "$knbn_board/$arg_column")" #Get task string excluding the line nr
+        task_line=${task_string:0:3} #Extract line nr
+        task_string=${task_string:2}
+        printf "%s\\n" "${task_string//*> /tmp> }" >> "$knbn_board/${new_column,,}" #Insert task into new column with temporary ID
+        rm_notes "$((${task_line%:*}+1))" "$arg_column" "${new_column,,}"
+        sed -i "/${4}> /d" "$knbn_board/$arg_column" #Delete task from old column
+        [[ -z "$(< "$knbn_board/$arg_column")" ]] && rm "${knbn_board:?}/$arg_column"
+        regenerate_numbers "$arg_column"
+        regenerate_numbers "$new_column"
+        knbn "ls"
     ;;
     "rm"|"-d"|"--remove") #Syntax: knbn rm column task_id
-        if ! [[ -z "$2" &&  -z "$2" ]]; then
-            rm_column="${2,,}" #Make all characters lowercase
-            rm_column="${rm_column// /_}" #Replace all spaces with an underscore to prevent catastrophic failure
-            note_line="$(grep -n "$3> " "$knbn_board/$rm_column")"
-            note_line=${note_line:0:3}
-            grep -q "$3> " "$knbn_board/$rm_column" && \
-                rm_notes "$((${note_line%:*}+1))" "$rm_column" && \
-                sed -i "/${3}> /d" "$knbn_board/$rm_column" #Delete notes first, then tasks if id exists in column
-            file="$(< "$knbn_board/$rm_column")" #Get file contents of column
-            [[ -z $file ]] && rm "${knbn_board:?}/$rm_column" #Delete file if column is empty
-            regenerate_numbers "$rm_column" #Regenerate ids
-            knbn "ls"
-        fi
+        [[ -z "$2" || -z "$3" ]] && exit 1
+
+        ! grep -q "$3> " "$knbn_board/$arg_column" && exit 1
+        rm_notes "$((task_line+1))" "$arg_column"
+        sed -i "/${3}> /d" "$knbn_board/$arg_column" #Delete notes first, then tasks if id exists in column
+        [[ -z "$(< "$knbn_board/$arg_column")" ]] && rm "${knbn_board:?}/$arg_column" #Delete file if column is empty
+        regenerate_numbers "$arg_column" #Regenerate ids
+        knbn "ls"
     ;;
     "rmnt"|"-r"|"--remove-note")
-        if ! [[ -z "$2" && -z "$3" && -z "$4" ]]; then
-            rmnt_column="${2,,}" #Make all characters lowercase
-            rmnt_column="${rmnt_column// /_}" #Replace all spaces with an underscore to prevent catastrophic failure
-            if grep -q "$3> " "$knbn_board/$rmnt_column"; then #Check if id exists in column
-                task_line=$(grep -n "${3}> " "$knbn_board/$rmnt_column") #Get string of id prepended by line nr
-                task_line=${task_line:0:3} #Extract line nr
-                note_line=$(( ${task_line%:*} + $4 )) #Adjust line number from task to note
-                get_note="$(sed -n "${note_line}p" "$knbn_board/$rmnt_column")" #Get string of line
-                [[ ${get_note:0:4} == "  - " ]] && \
-                    sed -i "${note_line}d" "$knbn_board/$rmnt_column" #Delete note if the line being check is a note
-                knbn "ls"
-            fi
-        else
-            printf "%s\\n" "Syntax: knbn rmnt [column] [task id] [line offset]"
-        fi
+        [[ -z "$2" || -z "$3" || -z "$4" ]] && \
+            printf "%s\\n" "Syntax: knbn rmnt [column] [task id] [line offset]" && exit 1
+
+        ! grep -q "$3> " "$knbn_board/$arg_column" || ! [[ -f "$knbn_board/$arg_column" ]] && exit 1
+
+        task_line=$(grep -n "$3> " "$knbn_board/$arg_column") #Get string of id prepended by line nr
+        task_line=${task_line:0:3} #Extract line nr
+        note_line=$(( ${task_line%:*} + $4 )) #Adjust line number from task to note
+        get_note="$(sed -n "${note_line}p" "$knbn_board/$arg_column")" #Get string of line
+        [[ ${get_note:0:4} == "  - " ]] && \
+            sed -i "${note_line}d" "$knbn_board/$arg_column" #Delete note if the line being check is a note
+        knbn "ls"
     ;;
     "wipe"|"-w"|"--wipe")
-        wipe_column="${2,,}" #Make all characters lowercase
-        [[ -f "$knbn_board/${wipe_column// /_}" ]] && rm "$knbn_board/${wipe_column// /_}" #Delete column if it exists
+        [[ -f "$knbn_board/$arg_column" ]] && rm "$knbn_board/$arg_column" #Delete column if it exists
         knbn "ls"
     ;;
     "backup"|"-b"|"--backup")
-        if ! [[ -z "$2" ]]; then
-            cp -r "$knbn_board/." "$2" #Copy all columns to specified path
-            printf "%s\\n" "All columns have been saved to $2"
-        fi
+        [[ -z "$2" ]] && exit 1
+        cp -r "$knbn_board/." "$2" #Copy all columns to specified path
+        printf "%s\\n" "All columns have been saved to $2"
     ;;
     "restore"|"-R"|"--restore")
-        if ! [[ -z "$2" ]]; then
-            read -r -s -p "Are you sure you want to restore from backup? This will remove everything from the board. (y/N)" -n 1 CONFIRM
-            printf "%s\\n" ""
-            [[ ${CONFIRM,,} == "y" ]] && \
-                rm -rf "${knbn_board:?}/*" && \
-                cp -r "$2/." "$knbn_board" #Delete all columns then copy all columns in backup folder to board
-            printf "%s\\n" "knbnBrd has restored from $2"
-        fi
+        [[ -z "$2" ]] && exit 1
+        read -r -s -p "Are you sure you want to restore from backup? This will remove everything from the board. (y/N)" -n 1 CONFIRM
+        printf "%s\\n" ""
+        [[ ${CONFIRM,,} == "y" ]] && \
+            rm -rf "${knbn_board:?}/*" && \
+            cp -r "$2/." "$knbn_board" #Delete all columns then copy all columns in backup folder to board
+        printf "%s\\n" "knbnBrd has restored from $2"
     ;;
-    "uninstall"|"-u"|"--uninstall") #Uninstall knbnBrd entirely
+    "uninstall"|"-u"|"--uninstall")
         rm -rf /opt/knbnBrd
         rm /usr/bin/knbn
     ;;
     "license"|"-L"|"--license")
-        printf "\\n%s\\n" "$(< $LICENSE)" #Print the license
+        printf "\\n%s\\n" "$(< $LICENSE)"
     ;;
     "notice"|"-N"|"--notice")
-        printf "\\n%s\\n" "$(< $NOTICE)" #Print the notice
-        printf "\\n%s\\n\\nv2.0\\n\\n" "Type knbn license to view the entire license, knbn help to view options"
+        printf "\\n%s\\n" "$(< $NOTICE)"
     ;;
     "examples"|"-e"|"--examples")
-        printf "\\n%s\\n" "$(< $EXAMPLES)" #Print the examples
+        printf "\\n%s\\n" "$(< $EXAMPLES)"
     ;;
     "help"|"-h"|"--help"|*)
         printf "\\n%s\\n" "Usage: knbn [option] [arguments]"
