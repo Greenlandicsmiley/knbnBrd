f1099cd6796bad411dfbdc043587ad9097cd1577
Thomas Petersen <67438283+Greenlandicsmiley@users.noreply.github.com>
2021-07-01 00:34:58 -0200

    HELP file addition and some testing
    Maybe it is possible to make this script smaller?
    HELP file has been added to aid in that.
    Still need to think about whether or not it is actually possible to shorten it.

--- knbn.sh
+++ knbn.sh
@@ -5,6 +5,7 @@ knbn_board="$knbn_directory/board"
 LICENSE="/usr/share/doc/knbnBrd/LICENSE"
 NOTICE="/usr/share/doc/knbnBrd/NOTICE"
 EXAMPLES="/usr/share/doc/knbnBrd/EXAMPLES"
+EXAMPLES="/usr/share/doc/knbnBrd/HELP"
 
 regenerate_numbers() { #Regenerate line numbers in given column
     ! [[ -f "$knbn_board/${1,,}" ]] && return 1 #Check if file exists
@@ -20,12 +21,21 @@ regenerate_numbers() { #Regenerate line numbers in given column
 }
 
 rm_notes() {
-    check_next_line="$(sed -n "${1}p" "$knbn_board/$2")" #Get string from line supplied by 1st argument
-    while [[ ${check_next_line:0:4} == "  - " ]]; do
-        ! [[ -z "$3" ]] && printf "%s\\n" "$check_next_line" >> "$knbn_board/$3" #Insert note into new column if 3rd argument is not empty
-        sed -i "${1}d" "$knbn_board/$2" #Delete note from old column
-        check_next_line="$(sed -n "${1}p" "$knbn_board/$2")"
+    end_line=$task_line
+    while [[ "$(sed -n "$(( end_line + 1 ))p" "$column_file")" == "  - "* ]]; do
+        end_line=$(( end_line + 1 ))
     done
+    ! [[ -z "$1" ]] && sed -n "${task_line},${end_line}p" "$column_file" >> "$knbn_board/${new_column,,}" && \
+        regenerate_numbers "$new_column"
+    sed -i "${task_line},${end_line}d" "$column_file"
+    [[ -z "$(< "$column_file")" ]] && rm "${column_file:?}"
+    regenerate_numbers "$arg_column"
+    knbn "ls"
+}
+
+check_grep() {
+    ! [[ -f "$column_file" ]] && return 0
+    ! grep -q "${1:-mt}> " "$column_file" && return 0
 }
 
 arg_column="${2,,}"
@@ -34,7 +44,9 @@ arg_column="${arg_column// /_}"
 column_file="$knbn_board/$arg_column"
 
 [[ -f "$column_file" ]] && task_string="$(grep -n "$3> " "$column_file")"
-[[ "$1" =~ ^("mv"|"-m"|"--move")$ ]] && task_string="$(grep -n "$4> " "$column_file")"
+[[ "$1" =~ ^("mv"|"-m"|"--move")$ ]] && \
+    task_string="$(grep -n "$4> " "$column_file")" && \
+    new_column="${3// /_}"
 task_line="${task_string:0:3}"
 task_line="${task_line%:*}"
 
@@ -47,18 +59,16 @@ case $1 in
         knbn "ls"
     ;;
     "nt"|"-n"|"--note")
-        [[ -z "$4" || -z "$3" ]] || ! [[ -f "$column_file" ]] || ! grep -q "$3> " "$column_file" && \
-            printf "%s\\n" "Syntax: knbn nt [column] [task id] [description]" && exit 1
+        [[ -z "$4" ]] && \
+            check_grep "$3" && \
+            printf "%s\\n" "Please check your syntax" && exit 1
 
-        task_line=$(( task_line + 1 ))
-        check_next_line="$(sed -n "${task_line}p" "$column_file")" #Get string of next line
-        while [[ ${check_next_line:0:4} == "  - " ]]; do #Check if string is a note
-            task_line=$(( task_line + 1 ))
-            check_next_line="$(sed -n "${task_line}p" "$column_file")"
+        end_line=$task_line
+        while [[ "$(sed -n "$(( end_line + 1 ))p" "$column_file")" == "  - "* ]]; do
+            end_line=$(( end_line + 1 ))
         done
-        task_line=$(( task_line - 1 ))
-        check_next_line="$(sed -n "${task_line}p" "$column_file")"
-        sed -i "${task_line}s_.*_${check_next_line}\\n  - ${4}_" "$column_file" #Add note to line
+        replace_string="$(sed -n "${end_line}p" "$column_file")"
+        sed -i "${end_line}s_.*_${check_next_line}\\n  - ${4}_" "$column_file" #Add note to line
         knbn "ls"
     ;;
     "ls"|"-l"|"--list")
@@ -71,7 +81,7 @@ case $1 in
                 [[ ${#text} -gt $padding_length ]] && padding_length=${#text} #Set padding length to longest string
             done
             text="| $column: "
-            [[ ${#text} -gt $padding_length ]] && padding_length=${#text} #Set padding length if task/note is not longer than column - this is to prevent cases where padding is not correct when column is longer than the tasks
+            [[ ${#text} -gt $padding_length ]] && padding_length=${#text} #This is to prevent cases where padding is not correct when column is longer than the tasks
         done
         printf -v "border" "%-${padding_length}s+" "+" #Need to store to variable to replace all whitespace with a hyphen
         for column in ${arg_column//,/ }; do
@@ -86,35 +96,23 @@ case $1 in
         printf "%s\\n" "${border// /-}"
     ;;
     "mv"|"-m"|"--move")
-        [[ -z "$3" || -z "$4" ]] || ! [[ -f "$column_file" ]] || ! grep -q "$4> " "$column_file" && \
+        check_grep "$4" && \
             printf "%s\\n" "Syntax: knbn mv [old column] [new column] [task id]" && exit 1
 
-        new_column="${3// /_}"
-        task_string=${task_string:2}
-        printf "%s\\n" "${task_string//*> /tmp> }" >> "$knbn_board/${new_column,,}"
-        rm_notes "$(( task_line + 1 ))" "$arg_column" "${new_column,,}"
-        sed -i "/${4}> /d" "$column_file"
-        [[ -z "$(< "$column_file")" ]] && rm "${column_file:?}"
-        regenerate_numbers "$arg_column"
-        regenerate_numbers "$new_column"
-        knbn "ls"
+        rm_notes ","
     ;;
     "rm"|"-d"|"--remove")
-        [[ -z "$3" ]] || ! [[ -f "$column_file" ]] || ! grep -q "$3> " "$column_file" && \
+        check_grep "$3" && \
             printf "%s\\n" "Syntax: knbn rm [column] [task id]" && exit 1
 
-        rm_notes "$(( task_line + 1 ))" "$arg_column"
-        sed -i "/${3}> /d" "$column_file"
-        [[ -z "$(< "$column_file")" ]] && rm "${column_file:?}"
-        regenerate_numbers "$arg_column"
-        knbn "ls"
+        rm_notes
     ;;
     "rmnt"|"-r"|"--remove-note")
-        [[ -z "$3" || -z "$4" ]] || ! [[ -f "$column_file" ]] || ! grep -q "$3> " "$column_file" && \
+        [[ -z "$4" ]] && \
+            check_grep "$3" && \
             printf "%s\\n" "Syntax: knbn rmnt [column] [task id] [line offset]" && exit 1
 
-        get_note="$(sed -n "$(( task_line + $4 ))p" "$column_file")"
-        [[ ${get_note:0:4} == "  - " ]] && \
+        [[ "$(sed -n "$(( task_line + $4 ))p" "$column_file")" == "  - "* ]] && \
             sed -i "$(( task_line + $4 ))d" "$column_file" && knbn "ls"
     ;;
     "wipe"|"-w"|"--wipe")
@@ -156,31 +154,6 @@ case $1 in
         printf "\\n%s\\n" "$(< "$EXAMPLES")"
     ;;
     "help"|"-h"|"--help"|*)
-        printf "\\n%s\\n" "Usage: knbn [option] [arguments]"
-        printf "\\n%s\\n" "Tasks"
-        printf "  %s\\n    %s\\n" "add, -a, --add [column] [description]" "Add a task to a column"
-        printf "  %s\\n    %s\\n" "ls, -l, --list [column],..." "List tasks in selected columns"
-        printf "  %s\\n    %s\\n" "ls, -l, --list" "List all tasks"
-        printf "  %s\\n    %s\\n" "mv, -m, --move [old column] [new column] [task id]" "Move a task"
-        printf "  %s\\n    %s\\n" "rm, -d, --remove [column] [task id]" "Delete a task from a column"
-        printf "  %s\\n    %s\\n" "wipe, -w, --wipe [column]" "Delete all tasks in a column"
-
-        printf "\\n%s\\n" "Notes"
-        printf "  %s\\n    %s\\n" "nt, -n, --note [column] [task id] [description]" "Add a note to a task"
-        printf "  %s\\n    %s\\n" "rmnt, -r, --remove-note [column] [task id] [line offset]" "Delete a note from a task"
-
-        printf "\\n%s\\n" "Backup and restore"
-        printf "  %s\\n    %s\\n" "backup, -b, --backup [column] [backup location]" "Backup the kanban board"
-        printf "  %s\\n    %s\\n" "restore, -R, --restore [backup location]" "Restore from backup"
-
-        printf "\\n%s\\n" "Other"
-        printf "  %s\\n    %s\\n" "uninstall, -u, --uninstall" "Uninstall knbnBrd"
-        printf "  %s\\n    %s\\n" "license, -L, --license" "Print license"
-        printf "  %s\\n    %s\\n" "notice, -N, --notice" "Print GPLv3 notice and contact information"
-        printf "  %s\\n    %s\\n" "help, -h, --help" "Print this"
-        printf "  %s\\n    %s\\n" "examples, -e, --examples" "Print examples"
-        printf "  %s\\n    %s\\n\\n" "--create-dir" "Create knbn board directory"
-        printf "%s\\n" "Contact the author at greenlandicsmiley@gmail.com"
-        printf "%s\\n\\n" "Or via reddit www.reddit.com/u/Greenlandicsmiley"
+        printf "\\n%s\\n" "$(< "$HELP")"
     ;;
 esac
